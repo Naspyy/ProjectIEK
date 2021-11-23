2877c40cfacaad8f2efe9ee10ec464c05f34e336
spyro <spyro@LAPTOP-GCVCKK1J>
2021-11-23 20:49:54 +0200

    Main Class

--- src/main/java/iek/neaionia/projectiek/Player.java
+++ src/main/java/iek/neaionia/projectiek/Player.java
@@ -19,6 +19,22 @@ public class Player {
   public Player (String f, String l) {
       this.firstName = f;
       this.lastName = l;
-  }
+  }  
+ //TODO Να φτιαξω μεθοδο για υπολογισμο ηλικιας απο ημερομηνια
+ //TODO Να φτιαξω μεθοδο για προσθηκη καλαθιων
+  
+  public void printStats()
+  {
+      System.out.println("First Name: " + firstName);
+      System.out.println("Last Name: " + lastName);
+      System.out.println("Birth Date: " + birthDate);
+       
+      
+      }
+ 
+      
+  
+      
+  
     
 }
---
+++ src/main/java/iek/neaionia/projectiek/PlayerMain.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iek.neaionia.projectiek;
+
+/**
+ *
+ * @author spyro
+ */
+public class PlayerMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      Player player1 = new Player("Εξω","Φυλαρουχας");
+        
+        
+        // TODO code application logic here
+    }
+    
+}
